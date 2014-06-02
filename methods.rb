b7b0ffe73fd113f2a43e64cc05156bd27cae4135
justin veirs <justinveirs@gmail.com>
2014-06-01 20:31:41 -0600

    added some text

---
+++ methods.rb
@@ -0,0 +1,9 @@
+#Create a new file called methods.rb to work on this exercise.
+# Create a Person class
+# Create three methods for your person:
+# Greet displays a greeting.
+# Joke displays a joke.
+# Fact displays a fact.
+# Create an instance of the class and call all three methods on it.
+array = [1, 2, 3]
+array.each {|number| number += 1}
